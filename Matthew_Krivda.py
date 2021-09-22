f96e39496d1f37108658fbf8e4757c2073aef63a
M8hew <makrivda@edu.hse.ru>
2021-09-22 10:52:37 +0300

    Method to solve leetcode task "maximum-69-number"

---
+++ Matthew_Krivda.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        num = str(num)
+        ind = num.find('6')
+        if ind==-1:
+            return int(num)
+        else:
+            return int(num[:ind]+'9'+num[ind+1:])
