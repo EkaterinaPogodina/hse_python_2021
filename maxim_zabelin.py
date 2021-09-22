1c5f2b80aa548b7cc674e57cc5acd92700d79ea6
maximzabelin <maxzabelin32@gmail.com>
2021-09-22 12:43:51 +0300

    new solution of 1 task

--- maxim_zabelin.py
+++ maxim_zabelin.py
@@ -1,22 +1,15 @@
-def isSelfD(num):
-    num_s = str(num)
-    for char in num_s:
-        char = int(char)
-        if char == 0:
-            return False
-        if num % char != 0:
-            return False
-    return True
+def isSelfD(left, right):
+    res = []
+    for num in range(left, right + 1):
+        num_s = str(num)
+        key = True
+        for char in num_s:
+            char = int(char)
 
-
-s = input().split(',')
-for i in range(len(s)):
-    s[i] = int(s[i].split()[2])
-
-
-res = []
-for n in range(s[0], s[1] + 1):
-    if isSelfD(n):
-        res.append(n)
-
-print(res)
+            if char == 0:
+                key = False
+            elif num % char != 0:
+                key = False
+        if key:
+            res.append(num)
+    return res
