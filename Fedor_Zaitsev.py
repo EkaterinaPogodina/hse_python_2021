4d09dc586a0c7e4bdd79f968302b6d826484ced9
FedorZaitsev <fvzaytsev@edu.hse.ru>
2021-09-23 11:11:02 +0300

    Task #1 solution Zaitsev Fedor

--- Fedor_Zaitsev.py
+++ Fedor_Zaitsev.py
@@ -1,16 +1,19 @@
-def change_digit(number, digit_num):
-    if number // 10**digit_num - number // 10**(digit_num + 1) * 10 == 6:
-        number += 3 * 10**digit_num
+def change_digit(number, dig_num):
+    if number // 10**dig_num - number // 10**(dig_num + 1) * 10 == 6:
+        number += 3 * 10**dig_num
 
-    if number // 10 ** digit_num - number // 10 ** (digit_num + 1) * 10 == 9:
-        number += 3 * 10 ** digit_num
+    elif number // 10 ** dig_num - number // 10 ** (dig_num + 1) * 10 == 9:
+        number -= 3 * 10 ** dig_num
 
     return number
 
 
 def max_69_number(input_number):
     ans = input_number
-    for i in range(4):
+    for i in range(5):
         ans = max(ans, change_digit(input_number, i - 1))
     return ans
 
+
+num = int(input())
+print(max_69_number(num))
