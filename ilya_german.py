50f1c71995abede29b4151aa6d43746239e4b574
ilyagerman52 <ivgerman@edu.hse.ru>
2021-09-23 14:00:58 +0300

    🐧

--- ilya_german.py
+++ ilya_german.py
@@ -1,8 +1,7 @@
-def my_func(nums):
+def num_identical_pairs(nums):
     counter = 0
     for i in range(0, len(nums) - 1):
         for j in range(i+1, len(nums)):
             if nums[i] == nums[j]:
                 counter += 1
-    print(counter)
     return counter
