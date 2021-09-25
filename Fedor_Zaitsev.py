9e5c057390861b428eadd03840e37c4a3c68ff3d
FedorZaitsev <fvzaytsev@edu.hse.ru>
2021-09-25 20:13:25 +0300

    Task 1 small function solution by Fedor Zaitsev 25.09

--- Fedor_Zaitsev.py
+++ Fedor_Zaitsev.py
@@ -1,16 +1,6 @@
-def change_dig(number, dig_num):
-    if number // 10**dig_num - number // 10**(dig_num + 1) * 10 == 6:
-        number += 3 * 10**dig_num
-
-    elif number // 10 ** dig_num - number // 10 ** (dig_num + 1) * 10 == 9:
-        number -= 3 * 10 ** dig_num
-    return number
-
-
-def max_69_number(input_number):
-    ans = input_number
-    for i in range(5):
-        ans = max(ans, change_dig(input_number, i - 1))
+def max_69_number(num):
+    ans = num
+    for i in range(4):
+        new_num = num // 10**(i + 1) * 10**(i + 1) + 9 * 10**i + num % 10**i
+        ans = max(ans, new_num)
     return ans
-
-
