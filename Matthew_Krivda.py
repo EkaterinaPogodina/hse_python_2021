abfbc9b1f13b391fe93f5604147d5ca58be029d3
M8hew <makrivda@edu.hse.ru>
2021-09-26 13:02:26 +0300

    correspondence to Pep8

--- Matthew_Krivda.py
+++ Matthew_Krivda.py
@@ -1,8 +1,8 @@
 class Solution:
-    def maximum69Number (self, num: int) -> int:
+    def maximum69Number(self, num: int) -> int:
         num = str(num)
         ind = num.find('6')
-        if ind==-1:
+        if ind == -1:
             return int(num)
         else:
-            return int(num[:ind]+'9'+num[ind+1:])
+            return int(num[:ind] + '9' + num[ind + 1:])
